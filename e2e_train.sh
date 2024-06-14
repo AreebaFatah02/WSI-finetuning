b39720d8493bfc5bde8708034fe495e88bf6ab23
Areeba Fatah <128526544+AreebaFatah02@users.noreply.github.com>
2024-06-14 08:52:46 +0500

    Update e2e_train.sh

--- e2e_train.sh
+++ e2e_train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=4 python train_end2end.py --exp_code 'FT_Camelyon16_res50' --k_start 0 --k_end 5 --bag_size 512 --data_root_dir '/kaggle/working/WSI-finetuning/data_feat/Camel16_ostu_top512_vib'
+CUDA_VISIBLE_DEVICES=4 python train_end2end.py --exp_code 'clam_cam16_res50_ostu_init' --k_start 0 --k_end 5 --bag_size 512 --data_root_dir '/kaggle/working/WSI-finetuning/data_feat/Camel16_ostu_top512_vib'
